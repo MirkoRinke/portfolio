a9d5122afbf2991a98adbdf4d734f811deee3b1a
Mirko Rinke <mirkorinke@htp.com>
2024-11-23 02:24:19 +0100

    Add About Me component with language support and update styles

---
+++ src/app/main-content/about-me/about-me.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { LanguageService } from '../../shared/services/language.service';
+import { textsDE, textsEN } from './language';
+
+@Component({
+  selector: 'app-about-me',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-me.component.html',
+  styleUrl: './about-me.component.scss',
+})
+export class AboutMeComponent {
+  isHovered = false;
+  texts: any = {};
+  private languageSubscription: Subscription | undefined;
+
+  constructor(private languageService: LanguageService) {}
+
+  onMouseOver() {
+    this.isHovered = true;
+  }
+
+  ngOnInit() {
+    this.languageSubscription =
+      this.languageService.selectedLanguage$.subscribe((language) => {
+        this.loadTexts(language);
+      });
+    this.loadTexts(this.languageService.getLanguage());
+  }
+
+  ngOnDestroy() {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
+  }
+
+  loadTexts(language: string) {
+    if (language === 'de') this.texts = textsDE;
+    else if (language === 'en') this.texts = textsEN;
+    else this.texts = textsEN;
+  }
+}
---
+++ src/app/main-content/about-me/language.ts
@@ -0,0 +1,25 @@
+export const textsDE = {
+  whoIAm: 'Wer ich bin',
+  aboutMe: 'Über mich',
+  introduction:
+    'Hallo, ich bin ein deutschsprachiger Frontend-Entwickler mit Sitz in München. Motiviert von den grenzenlosen Möglichkeiten in der IT, freue ich mich darauf, visuell ansprechende und intuitive Websites und Anwendungen zu gestalten.',
+  workEnvironment:
+    'Flexibel in Bezug auf Arbeitsumgebungen, kann ich sowohl vor Ort in München als auch remote effektiv arbeiten.',
+  personalGrowth:
+    'Ich bin aufgeschlossen und suche immer nach persönlichen Herausforderungen, um mein Wissen und meine Fähigkeiten ständig zu verbessern.',
+  problemSolving:
+    'In meinem Beruf geht es beim Programmieren nicht nur darum, Code zu schreiben, sondern um eine kreative Form des Problemlösens. Ich bin stolz auf meine Fähigkeit, komplexe technische Herausforderungen in einfache, benutzerfreundliche Lösungen zu destillieren. Auf diese Weise helfe ich Ihnen, Ihre Ziele zu erreichen und Ihre Visionen zum Leben zu erwecken.',
+};
+
+export const textsEN = {
+  whoIAm: 'Who I Am',
+  aboutMe: 'About me',
+  introduction:
+    'Hi, I’m a german speaking Frontend Developer based in Munich. Motivated by the limitless opportunities within IT, I am excited about crafting visually captivating and intuitive websites and applications.',
+  workEnvironment:
+    'Flexible in terms of working environments, I can work effectively both on-site in Munich and remotely.',
+  personalGrowth:
+    'I am open-minded and always looking for personal challenges to constantly improve my knowledge and skills',
+  problemSolving:
+    'In my profession, programming isn t just about writing code it s a creative form of problem-solving. I take pride in my ability to distill complex technical challenges into simple, user-friendly solutions. This way, I help you achieve your goals and bring your visions to life.',
+};
