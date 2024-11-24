97c329e59c966d64504fda123a8c951eb3687195
Mirko Rinke <mirkorinke@htp.com>
2024-11-24 16:32:33 +0100

    Add visibility property to projects and update featured projects component for conditional rendering

--- src/app/main-content/featured-projects/projects.data.ts
+++ src/app/main-content/featured-projects/projects.data.ts
@@ -2,6 +2,7 @@ export const projectsEN = [
   {
     id: 1,
     name: 'Join',
+    visibility: true,
     about: 'What is this project about?',
     description:
       'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
@@ -20,6 +21,7 @@ export const projectsEN = [
   {
     id: 2,
     name: 'El Pollo Loco',
+    visibility: true,
     about: 'What is this second project about?',
     description:
       'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
@@ -37,6 +39,7 @@ export const projectsEN = [
   {
     id: 3,
     name: 'Pokedex',
+    visibility: true,
     about: 'What is this third project about?',
     description:
       'Pokedex application to search for Pokemon and get detailed information about them. The data is fetched from an external API.',
@@ -58,6 +61,7 @@ export const projectsDE = [
   {
     id: 1,
     name: 'Join',
+    visibility: true,
     about: 'Worum geht es bei diesem Projekt?',
     description:
       'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
@@ -76,6 +80,7 @@ export const projectsDE = [
   {
     id: 2,
     name: 'El Pollo Loco',
+    visibility: true,
     about: 'Worum geht es bei diesem zweiten Projekt?',
     description:
       'Jump-, Run- und Wurfspiel basierend auf einem objektorientierten Ansatz. Helfen Sie Pepe, Münzen und Tabasco-Salsa zu finden, um gegen das verrückte Huhn zu kämpfen.',
@@ -93,6 +98,7 @@ export const projectsDE = [
   {
     id: 3,
     name: 'Pokedex',
+    visibility: true,
     about: 'Worum geht es bei diesem dritten Projekt?',
     description:
       'Pokedex-Anwendung zum Suchen nach Pokemon und Abrufen detaillierter Informationen über sie. Die Daten werden von einer externen API abgerufen.',
