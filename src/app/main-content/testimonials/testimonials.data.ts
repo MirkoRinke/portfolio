ef3e2c5f978c2ec9258ea667bbcc0f2fae64833e
Mirko Rinke <mirkorinke@htp.com>
2024-11-26 19:50:26 +0100

    Add new testimonials for Project Manager and UX Designer; update HTML logic for active index handling

--- src/app/main-content/testimonials/testimonials.data.ts
+++ src/app/main-content/testimonials/testimonials.data.ts
@@ -17,6 +17,18 @@ export const testimonialsEN = [
     position: 'Backend Developer',
     text: "I learned enormously from Lukas' efficient way of working. A great addition to any team!",
   },
+  {
+    id: 4,
+    author: '- M. Becker',
+    position: 'Project Manager',
+    text: 'Lukas consistently delivered high-quality work and was always willing to go the extra mile to ensure project success.',
+  },
+  {
+    id: 5,
+    author: '- S. Wagner',
+    position: 'UX Designer',
+    text: 'Working with Lukas was a pleasure. His attention to detail and user-centric approach greatly improved our project outcomes.',
+  },
 ];
 
 export const testimonialsDE = [
@@ -37,5 +49,17 @@ export const testimonialsDE = [
     author: '- A. Müller',
     position: 'Backend-Entwickler',
     text: 'Ich habe enorm von Lukas’ effizienter Arbeitsweise gelernt. Eine großartige Bereicherung für jedes Team!',
+  },
+  {
+    id: 4,
+    author: '- M. Becker',
+    position: 'Projektmanager',
+    text: 'Lukas lieferte stets qualitativ hochwertige Arbeit und war immer bereit, einen zusätzlichen Schritt zu gehen, um den Projekterfolg sicherzustellen.',
+  },
+  {
+    id: 5,
+    author: '- S. Wagner',
+    position: 'UX-Designer',
+    text: 'Die Zusammenarbeit mit Lukas war eine Freude. Sein Augenmerk auf Details und sein nutzerzentrieren Ansatz haben unsere Projektergebnisse erheblich verbessert.',
   },
 ];
