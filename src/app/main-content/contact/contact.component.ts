c93363d09c860214ee499e64fe264fdbd37c3b29
Mirko Rinke <mirkorinke@htp.com>
2024-11-27 15:04:25 +0100

    Enhance contact form validation and placeholders; update styles for valid and invalid states

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  placeholderName: string = 'Your name goes here';
+  placeholderEmail: string = 'youremail@email.com';
+  placeholderMessage: string = 'Hello Lukas, I am interested in...';
 
+  placeholderNameClass: string = 'placeholder-valid';
+  placeholderEmailClass: string = 'placeholder-valid';
+  placeholderMessageClass: string = 'placeholder-valid';
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  onSubmit(NgForm: NgForm) {
+    if (NgForm.valid && NgForm.submitted) {
+      console.log('Form Submitted Successfully');
+    } else {
+      this.updatePlaceholders(NgForm);
+    }
+  }
+
+  updatePlaceholders(form: NgForm) {
+    if (form.controls['name'].invalid) {
+      this.placeholderName = 'Name is required';
+      this.contactData.name = '';
+      this.placeholderNameClass = 'placeholder-invalid';
+    }
+    if (form.controls['email'].invalid) {
+      this.placeholderEmail = 'Valid email is required';
+      this.contactData.email = '';
+      this.placeholderEmailClass = 'placeholder-invalid';
+    }
+    if (form.controls['message'].invalid) {
+      this.placeholderMessage = 'Message is required';
+      this.contactData.message = '';
+      this.placeholderMessageClass = 'placeholder-invalid';
+    }
+  }
 }
