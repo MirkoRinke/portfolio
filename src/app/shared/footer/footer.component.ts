86feba4b9967f4438259f5649070da19f45d5f9e
Mirko Rinke <mirkorinke@htp.com>
2024-11-27 21:05:49 +0100

    Implement multilingual support in footer component; update text bindings and add language service integration

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+import { LanguageService } from '../services/language.service';
+import { textsDE, textsEN } from './language';
+
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  texts: any = {};
+  private languageSubscription: Subscription | undefined;
 
+  constructor(private languageService: LanguageService) {}
+
+  ngOnInit() {
+    this.languageSubscription =
+      this.languageService.selectedLanguage$.subscribe((language) => {
+        this.loadTexts(language);
+      });
+    this.loadTexts(this.languageService.getLanguage());
+  }
+
+  ngOnDestroy() {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
+  }
+
+  loadTexts(language: string) {
+    if (language === 'de') this.texts = textsDE;
+    else if (language === 'en') this.texts = textsEN;
+    else this.texts = textsEN;
+  }
 }
---
+++ src/app/shared/footer/language.ts
@@ -0,0 +1,9 @@
+export const textsDE = {
+  position: 'Web-Entwickler',
+  location: 'Hildesheim Deutschland',
+};
+
+export const textsEN = {
+  position: 'Web Developer',
+  location: 'Hildesheim Germany',
+};
