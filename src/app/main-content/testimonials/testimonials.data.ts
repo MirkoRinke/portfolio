0c335b281af9f47b4d7c35bd221a5ae1e86295db
Mirko Rinke <mirkorinke@htp.com>
2024-12-02 16:29:21 +0100

    Refactor testimonials layout: update HTML structure for author and position display; adjust SCSS styles for improved responsiveness and alignment

--- src/app/main-content/testimonials/testimonials.data.ts
+++ src/app/main-content/testimonials/testimonials.data.ts
@@ -28,31 +28,31 @@ export interface Testimonial {
 export const testimonialsDE: Testimonial[] = [
   {
     id: 1,
-    author: '- H. Janisch',
+    author: 'H. Janisch',
     position: 'Team-Partner',
     text: 'Lukas hat sich als zuverlässiger Gruppenpartner erwiesen. Seine technischen Fähigkeiten und sein proaktiver Ansatz waren entscheidend für den Erfolg unseres Projekts.',
   },
   {
     id: 2,
-    author: '- T. Schulz',
+    author: 'T. Schulz',
     position: 'Frontend-Entwickler',
     text: 'Ich hatte das Glück, mit Lukas an einem Projekt bei der Developer Akademie zu arbeiten. Er blieb immer ruhig und sorgte dafür, dass unser Team auf Erfolgskurs war.',
   },
   {
     id: 3,
-    author: '- A. Müller',
+    author: 'A. Müller',
     position: 'Backend-Entwickler',
     text: 'Ich habe enorm von Lukas’ effizienter Arbeitsweise gelernt. Eine großartige Bereicherung für jedes Team!',
   },
   {
     id: 4,
-    author: '- M. Becker',
+    author: 'M. Becker',
     position: 'Projektmanager',
     text: 'Lukas lieferte stets qualitativ hochwertige Arbeit und war immer bereit, einen zusätzlichen Schritt zu gehen, um den Projekterfolg sicherzustellen.',
   },
   {
     id: 5,
-    author: '- S. Wagner',
+    author: 'S. Wagner',
     position: 'UX-Designer',
     text: 'Die Zusammenarbeit mit Lukas war eine Freude. Sein Augenmerk auf Details und sein nutzerzentrieren Ansatz haben unsere Projektergebnisse erheblich verbessert.',
   },
@@ -71,31 +71,31 @@ export const testimonialsDE: Testimonial[] = [
 export const testimonialsEN: Testimonial[] = [
   {
     id: 1,
-    author: '- H. Janisch',
+    author: 'H. Janisch',
     position: 'Team Partner',
     text: 'Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project.',
   },
   {
     id: 2,
-    author: '- T. Schulz',
+    author: 'T. Schulz',
     position: 'Frontend Developer',
     text: 'I had the good fortune of working with Lukas on a project at the Developer Akademie. He always stayed calm and made sure our team was set up for success.',
   },
   {
     id: 3,
-    author: '- A. Müller',
+    author: 'A. Müller',
     position: 'Backend Developer',
     text: "I learned enormously from Lukas' efficient way of working. A great addition to any team!",
   },
   {
     id: 4,
-    author: '- M. Becker',
+    author: 'M. Becker',
     position: 'Project Manager',
     text: 'Lukas consistently delivered high-quality work and was always willing to go the extra mile to ensure project success.',
   },
   {
     id: 5,
-    author: '- S. Wagner',
+    author: 'S. Wagner',
     position: 'UX Designer',
     text: 'Working with Lukas was a pleasure. His attention to detail and user-centric approach greatly improved our project outcomes.',
   },
