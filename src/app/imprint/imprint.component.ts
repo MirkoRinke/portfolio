201180909d2704087c6a4675495cf0a77447f9e6
Mirko Rinke <mirkorinke@htp.com>
2024-12-02 18:37:08 +0100

    Implement language support in imprint and privacy policy components; add dynamic text loading based on selected language

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 
+import { Subscription } from 'rxjs';
+import { LanguageService } from '../shared/services/language.service';
+
 @Component({
   selector: 'app-imprint',
   standalone: true,
@@ -9,4 +12,30 @@ import { FooterComponent } from '../shared/footer/footer.component';
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
-export class ImprintComponent {}
+export class ImprintComponent {
+  language: string = '';
+
+  private languageSubscription: Subscription | undefined;
+
+  constructor(private languageService: LanguageService) {}
+
+  ngOnInit() {
+    this.languageSubscription =
+      this.languageService.selectedLanguage$.subscribe((language) => {
+        this.loadTexts(language);
+      });
+    this.loadTexts(this.languageService.getLanguage());
+  }
+
+  ngOnDestroy() {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
+  }
+
+  loadTexts(language: string) {
+    if (language === 'de') this.language = 'DE';
+    else if (language === 'en') this.language = 'EN';
+    else this.language = 'EN';
+  }
+}
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 
+import { Subscription } from 'rxjs';
+import { LanguageService } from '../shared/services/language.service';
+
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
@@ -10,6 +13,32 @@ import { FooterComponent } from '../shared/footer/footer.component';
   styleUrl: './privacy-policy.component.scss',
 })
 export class PrivacyPolicyComponent {
+  language: string = '';
+
+  private languageSubscription: Subscription | undefined;
+
+  constructor(private languageService: LanguageService) {}
+
+  ngOnInit() {
+    this.languageSubscription =
+      this.languageService.selectedLanguage$.subscribe((language) => {
+        this.loadTexts(language);
+      });
+    this.loadTexts(this.languageService.getLanguage());
+  }
+
+  ngOnDestroy() {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
+  }
+
+  loadTexts(language: string) {
+    if (language === 'de') this.language = 'DE';
+    else if (language === 'en') this.language = 'EN';
+    else this.language = 'EN';
+  }
+
   onAnchorClick(event: any) {
     event.preventDefault();
     const targetId = event.target.getAttribute('href').substring(1);
