08e16f11d77db6e577f20c5e43b15e5b8ee14bfb
Mirko Rinke <mirkorinke@htp.com>
2025-01-02 19:56:13 +0100

    add site navigation component

---
+++ src/app/shared/site-navigation/site-navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-navigation',
+  imports: [],
+  templateUrl: './site-navigation.component.html',
+  styleUrl: './site-navigation.component.scss'
+})
+export class SiteNavigationComponent {
+
+}
