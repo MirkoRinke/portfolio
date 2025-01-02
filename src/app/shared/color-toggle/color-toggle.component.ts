fcbab82f2127203581fc110d44e755edfa536e25
Mirko Rinke <mirkorinke@htp.com>
2025-01-03 00:14:28 +0100

    add color toggle component with functionality and styling

---
+++ src/app/shared/color-toggle/color-toggle.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-color-toggle',
+  imports: [CommonModule],
+  templateUrl: './color-toggle.component.html',
+  styleUrl: './color-toggle.component.scss',
+})
+export class ColorToggleComponent {
+  currentColor: string | null = null;
+
+  setColor(color: string) {
+    if (color === 'default') {
+      this.resetColor();
+    } else {
+      this.applyColor(color);
+    }
+  }
+
+  resetColor() {
+    if (this.currentColor) {
+      document.body.classList.remove(this.currentColor);
+      this.currentColor = null;
+      localStorage.removeItem('currentColor');
+    }
+  }
+
+  applyColor(color: string) {
+    if (this.currentColor) {
+      document.body.classList.remove(this.currentColor);
+    }
+    document.body.classList.add(color);
+    this.currentColor = color;
+    localStorage.setItem('currentColor', color);
+  }
+}
