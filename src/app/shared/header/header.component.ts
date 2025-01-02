3257479a0f51170220843a18799ff1a48cf6d1ad
Mirko Rinke <mirkorinke@htp.com>
2025-01-03 00:18:19 +0100

    add header component layout with language and color toggle integration

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { SiteNavigationComponent } from '../site-navigation/site-navigation.component';
+import { LanguageToggleComponent } from '../language-toggle/language-toggle.component';
+import { ColorToggleComponent } from '../color-toggle/color-toggle.component';
 
 @Component({
   selector: 'app-header',
-  imports: [SiteNavigationComponent],
+  imports: [
+    SiteNavigationComponent,
+    LanguageToggleComponent,
+    ColorToggleComponent,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
