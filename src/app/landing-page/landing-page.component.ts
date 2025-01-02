9a1323be9b7ac047c4bfc6327bc14c6d130c67b1
Mirko Rinke <mirkorinke@htp.com>
2025-01-02 20:03:20 +0100

    embed hero component in landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
