0d64c23e8c7041d7dbb3264db541d3c5c53e8438
Mirko Rinke <mirkorinke@htp.com>
2025-01-04 00:35:45 +0100

    implement hero section with new layout, styling, and contact button

--- src/app/landing-page/hero/hero.component.ts
+++ src/app/landing-page/hero/hero.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { SvgIconsService } from '../../shared/services/svg.icons.service';
+
 @Component({
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  constructor(public svgIconsService: SvgIconsService) {}
 }
