7cb9710c3e6d786a4af1363fef13f5f5e0a85d92
Mirko Rinke <mirkorinke@htp.com>
2025-01-06 01:21:27 +0100

    add Certificates component with dynamic rendering and styling

--- src/app/landing-page/expertise/certificates/certificates.component.ts
+++ src/app/landing-page/expertise/certificates/certificates.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
+import { SvgIconsService } from '../../../shared/services/svg.icons.service';
+
+import { type Certificate, CERTIFICATES } from './certificates.data';
+
 @Component({
   selector: 'app-certificates',
   imports: [],
   templateUrl: './certificates.component.html',
-  styleUrl: './certificates.component.scss'
+  styleUrl: './certificates.component.scss',
 })
 export class CertificatesComponent {
+  certificates: Certificate[] = CERTIFICATES;
 
+  constructor(public svgIconsService: SvgIconsService) {}
 }
