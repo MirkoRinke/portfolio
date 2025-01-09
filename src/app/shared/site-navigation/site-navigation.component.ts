2532c8bf40f3ad325a86babf6b1eaa724eac2a4e
Mirko Rinke <mirkorinke@htp.com>
2025-01-09 21:58:04 +0100

    Implement active link highlighting in site navigation and improve fragment tracking

--- src/app/shared/site-navigation/site-navigation.component.ts
+++ src/app/shared/site-navigation/site-navigation.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
 
 import { LanguageService } from '../services/language.service';
 
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-site-navigation',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './site-navigation.component.html',
   styleUrl: './site-navigation.component.scss',
 })
-export class SiteNavigationComponent {
-  constructor(public languageService: LanguageService) {}
+export class SiteNavigationComponent implements OnInit {
+  currentFragment: string = '';
+
+  constructor(public languageService: LanguageService, private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentFragment = this.router.url.split('#')[1] || '';
+      }
+    });
+  }
+
+  ngOnInit(): void {}
 }
