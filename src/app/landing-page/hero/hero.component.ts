1783fff0868deb17b89a928f323898c9d298b6a5
Mirko Rinke <mirkorinke@htp.com>
2025-01-09 22:00:56 +0100

    Add router link to contact button in hero component for improved navigation

--- src/app/landing-page/hero/hero.component.ts
+++ src/app/landing-page/hero/hero.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { SvgIconsService } from '../../shared/services/svg.icons.service';
 import { LanguageService } from '../../shared/services/language.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
