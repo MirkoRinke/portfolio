71401266840a713f92b89977123eaa0db40dcd28
Mirko Rinke <mirkorinke@htp.com>
2025-01-09 02:29:37 +0100

    Add navigation cursors for certificate modal and update layout

--- src/app/landing-page/expertise/certificates/certificate-modal/certificate-modal.component.ts
+++ src/app/landing-page/expertise/certificates/certificate-modal/certificate-modal.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input } from '@angular/core';
 
-import { SvgIconsService } from '../../../../shared/services/svg.icons.service';
-
 import { ModalService } from '../../../../shared/services/modal.service';
 
 @Component({
@@ -14,8 +12,5 @@ export class CertificateModalComponent {
   @Input() modal!: any;
   @Input() index!: number;
 
-  constructor(
-    public svgIconsService: SvgIconsService,
-    public modalService: ModalService
-  ) {}
+  constructor(public modalService: ModalService) {}
 }
