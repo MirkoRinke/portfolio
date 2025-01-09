b5bafc318fb914136d274ef498b4f9f117e1f7ae
Mirko Rinke <mirkorinke@htp.com>
2025-01-09 23:48:35 +0100

    Refactor skills component to integrate learning skills and enhance icon status handling

--- src/app/landing-page/expertise/skills/skills.data.ts
+++ src/app/landing-page/expertise/skills/skills.data.ts
@@ -2,6 +2,7 @@ export interface Skill {
   name: string;
   icon: string;
   alt: string;
+  status: string;
   visibility: boolean;
 }
 
@@ -10,90 +11,70 @@ export const TECH_SKILLS: Skill[] = [
     name: 'HTML',
     icon: 'HTML',
     alt: 'HTML Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'CSS',
     icon: 'CSS',
     alt: 'CSS Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'SCSS',
     icon: 'SCSS',
     alt: 'SCSS Logo',
-    visibility: true,
-  },
-  {
-    name: 'Material Design',
-    icon: 'materialDesign',
-    alt: 'Material Design Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'JavaScript',
     icon: 'javaScript',
     alt: 'JavaScript Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'TypeScript',
     icon: 'typeScript',
     alt: 'TypeScript Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'Angular',
     icon: 'angular',
     alt: 'Angular Logo',
-    visibility: true,
-  },
-  {
-    name: 'Firebase',
-    icon: 'firebase',
-    alt: 'Firebase Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'GIT',
     icon: 'GIT',
     alt: 'GIT Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'REST API',
     icon: 'API',
     alt: 'REST API Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'Docker',
     icon: 'docker',
     alt: 'Docker Logo',
-    visibility: true,
-  },
-  {
-    name: 'Node js',
-    icon: 'node',
-    alt: 'node js Logo',
-    visibility: true,
-  },
-  {
-    name: 'PHP',
-    icon: 'PHP',
-    alt: 'PHP Logo',
+    status: 'skill',
     visibility: true,
   },
   {
     name: 'Figma',
     icon: 'figma',
     alt: 'Figma Logo',
-    visibility: true,
-  },
-  {
-    name: 'Affinity Designer',
-    icon: 'affinityDesigner',
-    alt: 'Affinity Designer',
+    status: 'skill',
     visibility: true,
   },
 ];
@@ -103,12 +84,49 @@ export const LEARNING_SKILLS: Skill[] = [
     name: 'React',
     icon: 'react',
     alt: 'React Logo',
-    visibility: false,
+    status: 'learning',
+    visibility: true,
   },
   {
     name: 'Vue Js',
     icon: 'vue',
     alt: 'Vue Js Logo',
-    visibility: false,
+    status: 'learning',
+    visibility: true,
+  },
+  {
+    name: 'Node js',
+    icon: 'node',
+    alt: 'node js Logo',
+    status: 'learning',
+    visibility: true,
+  },
+  {
+    name: 'PHP',
+    icon: 'PHP',
+    alt: 'PHP Logo',
+    status: 'learning',
+    visibility: true,
+  },
+  {
+    name: 'Firebase',
+    icon: 'firebase',
+    alt: 'Firebase Logo',
+    status: 'learning',
+    visibility: true,
+  },
+  {
+    name: 'Affinity Designer',
+    icon: 'affinityDesigner',
+    alt: 'Affinity Designer',
+    status: 'learning',
+    visibility: true,
+  },
+  {
+    name: 'Material Design',
+    icon: 'materialDesign',
+    alt: 'Material Design Logo',
+    status: 'learning',
+    visibility: true,
   },
 ];
