c59304d00f6b0789aa81b0f53520cc4962f60b53
Mirko Rinke <mirkorinke@htp.com>
2025-01-12 21:37:53 +0100

    Update tap service to set default active tap to 'home' and adjust expertise component logic

--- src/app/shared/services/tap.service.ts
+++ src/app/shared/services/tap.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class TapService {
-  private activeTap: string | null = 'skills';
+  public activeTap: string = 'home';
 
   constructor() {}
 
