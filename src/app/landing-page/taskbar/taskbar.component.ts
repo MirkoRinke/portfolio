094064654f9461798a9c54bd7f527b368cf7c102
Mirko Rinke <mirkorinke@htp.com>
2025-01-24 15:15:41 +0100

    feat(taskbar): update logo link to use routerLink and integrate RouterModule

--- src/app/landing-page/taskbar/taskbar.component.ts
+++ src/app/landing-page/taskbar/taskbar.component.ts
@@ -9,9 +9,11 @@ import { ModalService } from '../../shared/services/modal.service';
 import { UtilityService } from '../../shared/services/utility.service';
 import { ClockComponent } from './clock/clock.component';
 
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-taskbar',
-  imports: [CommonModule, ClockComponent],
+  imports: [CommonModule, ClockComponent, RouterModule],
   templateUrl: './taskbar.component.html',
   styleUrls: ['./taskbar.component.scss'],
 })
