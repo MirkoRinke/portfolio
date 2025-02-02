8796ca41a1c42813805d3f97fa3e5c28f9c61e98
Mirko Rinke <mirkorinke@htp.com>
2025-02-02 18:58:04 +0100

    feat(project): add visibility check for QR code display in project component

--- src/app/landing-page/projects-section/project/project.component.ts
+++ src/app/landing-page/projects-section/project/project.component.ts
@@ -1,20 +1,36 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 import { LanguageService } from '../../../shared/services/language.service';
 
 import { SvgIconsService } from '../../../shared/services/svg.icons.service';
+
+import { CheckVisibilityService } from '../../../shared/services/check-visibility.service';
 
 @Component({
   selector: 'app-project',
   imports: [],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss',
+  providers: [CheckVisibilityService],
 })
-export class ProjectComponent {
+export class ProjectComponent implements AfterViewInit {
   @Input() index!: number;
+
+  @ViewChild('projectWrapper', { static: false }) projectWrapper!: ElementRef;
 
   constructor(
     public languageService: LanguageService,
-    public svgIconsService: SvgIconsService
+    public svgIconsService: SvgIconsService,
+    public checkVisibilityService: CheckVisibilityService
   ) {}
+
+  ngAfterViewInit() {
+    this.checkVisibilityService.setElement(this.projectWrapper);
+  }
 }
