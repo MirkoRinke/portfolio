5581d484e01eab9247d9ba40c7d2c575a631a317
Mirko Rinke <mirkorinke@htp.com>
2025-02-20 19:11:13 +0100

    feat(github-contributions): implement dynamic color setting based on user preference

--- src/app/shared/services/github-contributions.service.ts
+++ src/app/shared/services/github-contributions.service.ts
@@ -59,6 +59,57 @@ export class GithubContributionsService {
     return weeks;
   }
 
+  setColor(colorHex: string): string {
+    const currentColor = localStorage.getItem('currentColor');
+    switch (currentColor) {
+      case 'violett':
+        return this.setColorViolett(colorHex);
+      case 'blue':
+        return this.setColorBlue(colorHex);
+      default:
+        return colorHex;
+    }
+  }
+
+  setColorViolett(colorHex: string): string {
+    switch (colorHex) {
+      case '#9be9a8':
+        return '#D5BFFA';
+      case '#40c463':
+        return '#AA78F5';
+      case '#30a14e':
+        return '#8040D1';
+      case '#216e39':
+        return '#5B1DAD';
+      default:
+        return colorHex;
+    }
+  }
+
+  setColorBlue(colorHex: string): string {
+    switch (colorHex) {
+      case '#9be9a8':
+        return '#A5C7F0';
+      case '#40c463':
+        return '#4A90E2';
+      case '#30a14e':
+        return '#2A6CB3';
+      case '#216e39':
+        return '#1A4A7A';
+      default:
+        return colorHex;
+    }
+  }
+
+  // usedColors: string[] = [];
+
+  // addUniqueColor(colorHex: string) {
+  //   if (!this.usedColors.includes(colorHex)) {
+  //     this.usedColors.push(colorHex);
+  //   }
+  //   console.log('Used Colors:', this.usedColors);
+  // }
+
   convertDate(date: string) {
     if (this.languageService.getLanguage() === 'de') {
       return new Date(date).toLocaleDateString('de-DE', {
