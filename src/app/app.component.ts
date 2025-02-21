3bbecd8e6065c4e73283d87ec233d026a2600ef3
Mirko Rinke <mirkorinke@htp.com>
2025-02-21 17:49:36 +0100

    refactor(app): remove header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';
 
 import { Title, Meta } from '@angular/platform-browser';
 
@@ -9,7 +8,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
