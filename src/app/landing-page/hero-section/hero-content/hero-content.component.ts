ab7170a851c06ce21475e46818ae019e53331aee
Mirko Rinke <mirkorinke@htp.com>
2025-02-21 17:49:45 +0100

    feat(hero-section): add header component and adjust min-height for layout consistency

--- src/app/landing-page/hero-section/hero-content/hero-content.component.ts
+++ src/app/landing-page/hero-section/hero-content/hero-content.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { SvgIconsService } from '../../../shared/services/svg.icons.service';
 import { LanguageService } from '../../../shared/services/language.service';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../../shared/header/header.component';
 
 @Component({
   selector: 'app-hero-section-content',
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './hero-content.component.html',
   styleUrl: './hero-content.component.scss',
 })
